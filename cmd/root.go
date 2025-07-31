e48e22fa6a0eeb3172d77ccb5832a8ed46e750b1
cristianoliveira <contato@cristianoliveira.com.br>
2025-07-31 22:42:06 +0200

    feat(ws-handler): adds command and instructions on how to use it
    This command handles when the scratchpad workspace gets the focus (which shouldn't happen). It will move focus back to the last focused workspace and take the focused window to that workspace too.
    This allow you to use different tools to focus windows in scratchpad, like notifications, external launchers, etc., and behave as "summoning" the window to the current workspace instead of focusing the scratchpad workspace.
    
    Add this snippet in your `~/.aerospace.toml` config:
    ```toml
    exec-on-workspace-change = ['/bin/bash', '-c',
        'aerospace-scratchpad workspace-handler $AEROSPACE_FOCUSED_WORKSPACE'
    ]

--- cmd/root.go
+++ cmd/root.go
@@ -34,7 +34,7 @@ https://i3wm.org/docs/userguide.html#_scratchpad
 	rootCmd.AddCommand(SummonCmd(aerospaceClient))
 	rootCmd.AddCommand(NextCmd(aerospaceClient))
 	rootCmd.AddCommand(InfoCmd(aerospaceClient))
-	rootCmd.AddCommand(DaemonCmd(aerospaceClient))
+	rootCmd.AddCommand(WorkspaceHandlerCmd(aerospaceClient))
 
 	return rootCmd
 }
